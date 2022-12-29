50ad54783056c005b61b0457ff2db4f282b0ba52
Marcel Straub <m@straubs.eu>
2022-12-29 16:48:42 +0100

    Moved from viper to koanf for configuration handling

--- src/restService/configuration/configuration.go
+++ src/restService/configuration/configuration.go
@@ -1,33 +1,44 @@
 package configuration
 
 import (
-	"github.com/spf13/viper"
+	"strings"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/dotenv"
+	"github.com/knadh/koanf/providers/confmap"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
 	"go.uber.org/zap"
 )
 
 type Config struct {
-	RestListener string `mapstructure:"REST_LISTENER"`
-	DBUrl        string `mapstructure:"DB_URL"`
+	RestListener string `mapstructure:"REST_LISTENER" koanf:"rest_listener"`
+	DBUrl        string `mapstructure:"DB_URL" koanf:"db_url"`
 }
 
 func LoadConfig(logger zap.Logger) (c Config, err error) {
-	viper.SetEnvPrefix("SMARTHOME")
-	viper.AddConfigPath("envs")
-	viper.SetConfigName("dev")
-	viper.SetConfigType("env")
-	viper.AutomaticEnv()
+	const envPrefix = "SMARTHOME_"
+	var k = koanf.New(".")
 
-	err = viper.ReadInConfig()
-	if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-		logger.Error("Failed to process configuration: " + err.Error())
-		return c, err
+	// set defaults
+	k.Load(confmap.Provider(map[string]interface{}{
+		"rest_listener": ":7777",
+		"db_url":        "UNCONFIGURED",
+	}, ","), nil)
+
+	// load configuration fom environment file
+	if err := k.Load(file.Provider("envs/dev"), dotenv.Parser()); err != nil {
+		logger.Error("failed to load configuration from file: " + err.Error())
 	}
 
-	err = viper.Unmarshal(&c)
-	if err != nil {
-		logger.Error("Failed to unmarshal configuration: " + err.Error())
-		return c, err
-	}
+	// load configuration from environment variables
+	k.Load(env.ProviderWithValue(envPrefix, ".", func(s string, v string) (string, interface{}) {
+		key := strings.ToLower(strings.TrimPrefix(s, envPrefix))
+		return key, v
+	}), nil)
+
+	println(k)
+	err = k.Unmarshal("", &c)
 
 	return c, err
 }
